9428d05e13d7372b60b6e219650d50db17a97046
Nil <nilbesombes@gmail.com>
2019-06-06 14:14:59 +0200

    map doesnt work cause of Google new price policiy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Flat from './components/flat';
+import GoogleMapReact from 'google-map-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      })
+
+  };
+
+
+
+  render () {
+      const center = {
+    lat: 48.8566,
+    lng: 2.3522,
+  }
+
+
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+          <input 
+          type="text"
+          placeholder="Rechercher..."
+          value={this.state.search}
+          onChange={this.handleSearch} />
+          </div>
+          <div className="flats">
+            {this.state.flats.map(function(flat) {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+         <GoogleMapReact
+           //  bootstrapURLKeys={{ key: "AIzaSyDG2OHW4fT4dkUVhNFge_jgLDfsbizPhu0" }}
+            center={center}
+            zoom={12}
+          >
+
+          </GoogleMapReact>
+        </div>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1> Hello tarass </h1>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Hello from './components/hello';
+// import Hello from './components/hello';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
